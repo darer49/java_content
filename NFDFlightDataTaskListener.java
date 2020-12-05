5545cae34e494388ef4af93da29d7527ccae0528
Pangchonghao <33894295+darer49@users.noreply.github.com>
2020-12-05 16:23:14 +0800

    javaweb

---
+++ NFDFlightDataTaskListener.java
@@ -0,0 +1,19 @@
+package com.TimerController;
+
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class NFDFlightDataTaskListener implements  ServletContextListener {
+
+   public void contextInitialized(ServletContextEvent sce) {
+        new TimerManager();
+   }
+
+   public void contextDestroyed(ServletContextEvent sce) {
+       // TODO Auto-generated method stub
+        
+   }
+
+}
